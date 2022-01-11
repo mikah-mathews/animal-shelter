07e231ce9e12fbe3bb7666e68d18c327be8ba111
Mikah Mathews <mikah@outofband.org>
2022-01-11 10:46:35 -0800

    add image selection and borders
    Co-Authored-By: Will Watkins <wjwat@onionslice.org>

--- js/styles.js
+++ js/styles.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
   $("button").click(function() {
     $("body").toggleClass("dark-background");
   });
+
+  $("img.pop").click(function() {
+    $(this).toggleClass("big-image");
+  });
 });
